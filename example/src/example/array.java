6fb0bafac95082e0db153842bfa84b2917da514d
lithiyamathew <lithiyamathew192002@gmail.com>
2023-11-07 11:18:05 +0530

    first commit

---
+++ example/src/example/array.java
@@ -0,0 +1,13 @@
+package example;
+
+public class array {
+
+	public static void main(String[] args) {
+String[] cars= {"bmw","suzuki","tata"};
+System.out.println(cars[0]);
+System.out.println(cars.length);
+cars[0]="mahindra";
+System.out.println(cars[0]);
+	}
+
+}
